299bfbccc2c7d0330e25825cd9c1a743d6e0fb1b
TEGME1 <shaikfahad3210@gmail.com>
2023-05-12 16:38:04 +0530

    final prototype

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {useEffect, useState} from "react";
 
 function App() {
-    const [gridSize, setGridSize] = useState(() => 9)
+    const [gridSize, setGridSize] = useState(() => 10)
     const [boxSize, setBoxSize] = useState(() => 50)
     const [position, setPosition] = useState(() => [0, 0])
     const move = (direction) => {
@@ -22,29 +22,43 @@ function App() {
         }
     }
 
+    const verticalButtonStyles = {
+        width: gridSize * boxSize
+    }
+
     return (
         <div className="App">
             <div className="container">
-                <button onClick={() => move(0)} className='up-button'>up</button>
-                <button onClick={() => move(3)} className='left-button'>left</button>
-                <button onClick={() => move(1)} className='right-button'>right</button>
-                <button onClick={() => move(2)} className='right-button'>down</button>
+                <button onClick={() => move(0)} className='up-button vertical-button' style={verticalButtonStyles}
+                        disabled={position[1] === 0}>Up
+                </button>
+                <section className='mid'>
+                    <button onClick={() => move(3)} className='left-button horizontal-button'
+                            disabled={position[0] === 0}>Left
+                    </button>
+                    <div style={{
+                        height: gridSize * boxSize,
+                        width: gridSize * boxSize
+                    }}>
+                        <div className='movable'
+                             style={{
+                                 height: boxSize,
+                                 width: boxSize,
+                                 transform: `translate(${boxSize * position[0]}px, ${boxSize * position[1]}px)`
+                             }}
+                        >
+                        </div>
+                    </div>
+                    <button onClick={() => move(1)} className='right-button horizontal-button'
+                            disabled={position[0] === gridSize - 1}>Right
+                    </button>
+                </section>
+                <button onClick={() => move(2)} className='down-button vertical-button'
+                        style={verticalButtonStyles} disabled={position[1] === gridSize - 1}
+                >Down
+                </button>
             </div>
-            <div style={{
-                backgroundColor: 'red',
-                height: gridSize * boxSize,
-                width: gridSize * boxSize
-            }}>
-                <div className='movable'
-                     style={{
-                         backgroundColor: 'blue',
-                         height: boxSize,
-                         width: boxSize,
-                         transform: `translate(${boxSize * position[0]}px, ${boxSize * position[1]}px)`
-                     }}
-                >
-                </div>
-            </div>
+
         </div>
     );
 }
