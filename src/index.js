cd83e19a9fbbb8449707939363b385fdc1f1f357
TEGME1 <shaikfahad3210@gmail.com>
2023-06-12 11:58:03 +0530

    Included hotjar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {hotjar} from 'react-hotjar'
 
+hotjar.initialize(3529714, 6)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
