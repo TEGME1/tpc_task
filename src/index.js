929bbe2c4f31ab5c17353f74d31282e764d62c50
TEGME1 <shaikfahad3210@gmail.com>
2023-06-12 12:33:34 +0530

    silly

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {hotjar} from 'react-hotjar'
+import Hotjar from '@hotjar/browser';
+
+const siteId = 3529714;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
